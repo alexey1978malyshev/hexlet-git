17ab4a5d6dac18c925f39ab3c7b16b7dbb401c28
Alexey Malyshev <alexey1978malyshev>
2020-10-16 16:37:53 +0300

    addscripts

--- first_script.py
+++ first_script.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python3
+
 def greet(who):
     print('Hello, {}!'.format(who))
 
-greet('Bob')
-greet('Ann')
+def main():
+    greet('Bob')
+    greet('Ann')
 
-
+if __name__ == '__main__':
+    main()
